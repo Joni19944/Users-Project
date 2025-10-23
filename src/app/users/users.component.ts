10b5b03e200ac2f73435cf99949262e8c7abbaaf
Joni Okriashvili <Joni.okriashvili94@gmail.com>
2025-10-23 07:49:09 +0400

    Add search bar

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,21 +1,39 @@
-import { Component, inject, input, OnInit, signal } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { FetchService } from '../services/fetch.service';
 import { RouterLink } from '@angular/router';
 import { User } from '../types/user';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css',
 })
 export class UsersComponent implements OnInit {
   fetchService = inject(FetchService);
   users = signal<User[]>([]);
+  searchTerm = signal('');
+
+  filteredUsers = computed(() => {
+    const term = this.searchTerm().toLowerCase();
+    return this.users().filter((user) => {
+      const [name, surname] = user.name.split(' ');
+      return (
+        name.toLowerCase().includes(term) ||
+        (surname && surname.toLowerCase().includes(term)) ||
+        user.email.toLowerCase().includes(term)
+      );
+    });
+  });
   ngOnInit(): void {
     this.fetchService
       .loadUsers()
       .subscribe({ next: (val) => this.users.set(val) });
+  }
+  updateSearch(event: Event) {
+    const value = (event.target as HTMLInputElement).value.toLowerCase();
+    this.searchTerm.set(value);
   }
 }
